71e08ed3b36504ef3ac6583b8412095a0612b6f7
Anh Nguyen <anhknguyen97@yahoo.com>
2020-06-14 16:59:06 -0700

    Convert PNG images to JPG

--- convertImages.js
+++ convertImages.js
@@ -8,3 +8,12 @@ Jimp.read("image.jpg", function (err, image) {
         image.write("new-image.png")
     }
 })
+
+// convert png to jpg
+Jimp.read("image.png", function (err, image) {
+    if (err) {
+        console.log(err)
+    } else {
+        image.write("new-image.jpg")
+    }
+})
